4f57d1350026705856374034e1c1fd0cda9476c3
hino tatsuya <ronnosippo@gmail.com>
2017-01-23 22:32:34 +0900

    タスク追加実装

--- todo-angular2/src/app/app.component.ts
+++ todo-angular2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  tasks = [
+    { name: '夕食の材料を買い物' ,enabled: true },
+    { name: '請求書の支払い'  ,enabled: true},
+    { name: 'ジムへ行く'  ,enabled: true},
+    { name: '洗濯物を取り込み'  ,enabled: true}
+  ];
+  newTask;
+
+  onClick(task: Task): void {
+    task.enabled = ! task.enabled;
+  }
+
+  addTask(): void {
+    if(this.newTask){
+      this.tasks.push({ name: this.newTask ,enabled: true});
+      this.newTask = '';
+    }
+  }
 }
